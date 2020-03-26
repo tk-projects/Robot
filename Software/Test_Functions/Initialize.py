886feb962fb201a99b88c67cdfe7d51c49315076
König <60255832+tk-projects@users.noreply.github.com>
2020-03-26 21:29:18 +0100

    Add files via upload

--- Software/Test_Functions/Initialize.py
+++ Software/Test_Functions/Initialize.py
@@ -9,6 +9,9 @@ Created on Sat Mar 21 13:47:50 2020
 # Load fuctions
 exec(open("/home/pi/Robot/LoadCalibration.py").read(), globals())
 exec(open("/home/pi/Robot/ServoControl.py").read(), globals())
+exec(open("/home/pi/Robot/LegControle.py").read(), globals())
+exec(open("/home/pi/Robot/RobotControl.py").read(), globals())
+exec(open("/home/pi/Robot/Robot.py").read(), globals())
 
 Servo=ServoClass()
 Cal=LoadCalibration('/home/pi/Robot/RobotSW/Calibration/ServoCalibration.txt')
@@ -23,15 +26,9 @@ def toRad(a):
     alpha_rad=a*2*np.pi/360
     return alpha_rad
 
-def stand(height):
-        h=height;   
-        # calculate alpha, angle btw upper thigh and body
-        h2 = (l1**2-l2**2-h**2)/(2*h);
-        h1 = h-h2;
-        
-        alpha=toDeg(np.arccos(h1/l2))+90;
-        beta=180-toDeg(np.arccos(h2/l2))-toDeg(np.arccos(h1/l2));
-        
-        return [alpha, beta]
+# Initialize Stand:
+#height=21.5   # height at init.
+#initStand(height)
 
-
+Robot=RobotClass()
+Robot.initStand()
